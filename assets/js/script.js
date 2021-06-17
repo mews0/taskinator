36d203acd4fc9cb62742be39e48e9dc03c0e0230
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-06-17 17:21:04 -0500

    add ability to delete a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
@@ -88,3 +90,19 @@ var createTaskActions = function(taskId) {
 }
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+
+  if (event.target.matches(".delete-btn")) {
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
